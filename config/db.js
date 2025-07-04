dace586f45056d0d34428f20d655035a23787e97
amrin-justnaik <amrin@justnaik.com>
2025-07-04 14:55:39 +0800

    feat: skip ssl

--- config/db.js
+++ config/db.js
@@ -8,8 +8,10 @@ const isProduction = process.env.NODE_ENV === 'production'
 // console.log('\n\n\n',process.env.DATABASE_URL,process.env.NODE_ENV,'\n\n\n')
 
 const Sql = new Pool({
-  connectionString: 'postgresql://justnaik:MDGvxKrhRWhj2C4HJUWE@justnaikdb.ckmfijp3sg7o.ap-southeast-1.rds.amazonaws.com:5432/justnaik',
-  ssl: false,
+    connectionString: process.env.DB_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
 })
 
 export default Sql;
